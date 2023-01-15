4538cd97d0bc337510e7de569584956976f83cda
Zifan Pan <brianpanzifan@gmail.com>
2023-01-15 02:06:10 -0500

    created scss general variables file and linked files

--- src/components/modules/portfolio-main.vue
+++ src/components/modules/portfolio-main.vue
@@ -1,27 +1,47 @@
 <template>
   <div class="portfolio-main">
-    <h1>Hello!</h1>
-    <h2>I'm Zifan Pan</h2>
+    <div class="portfolio-main__heading">
+      <div class="portfolio-main__heading-text">
+        <h1>Hello!</h1>
+        <h2>I'm Zifan Pan</h2>
+      </div>
+      <div class="portfolio-main__heading-icon-wrapper">
+        <!-- <span>Scroll</span> -->
+        <icon-chevron class="portfolio-main__heading-icon" :is-round="true" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+
+import IconChevron from "../widgets/icons/icon-chevron.vue";
+
 export default defineComponent({
   data() {
     return {};
+  },
+  components: {
+    IconChevron,
   },
 });
 </script>
 
 <style lang="scss" scoped>
-@import "../../styles/responsive.scss";
+@import "../../styles/variables.scss";
 
 .portfolio-main {
-  $border: 2px solid salmon;
+  border: 1px solid red;
 
   @media #{$bp-tablet} {
     background-color: red;
   }
+}
+
+.portfolio-main__heading-icon {
+  width: 25px;
+  height: 25px;
+  stroke-width: 3px;
 }
 </style>
