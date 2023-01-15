c55caaa617cf44aad8e07644106122a183323f09
Zifan Pan <brianpanzifan@gmail.com>
2023-01-14 23:40:09 -0500

    customized icon chevron vue file

---
+++ src/components/widgets/icons/icon-chevron.vue
@@ -0,0 +1,28 @@
+<template>
+  <svg
+    fill="none"
+    stroke="currentColor"
+    :stroke-linecap="isRound ? 'round' : ''"
+    :stroke-linejoin="isRound ? 'round' : ''"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    role="presentation"
+  >
+    <path d="M19 9l-7 7-7-7"></path>
+  </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    isRound: {
+      type: Boolean,
+      default: false,
+    },
+  },
+});
+</script>
+
+<style lang="less" scoped></style>
